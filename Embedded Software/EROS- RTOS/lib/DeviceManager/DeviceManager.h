3a4d59e56248eaacea7865fa4d59a8905ad38318
Paweł Czarzasty <278356@student.pwr.edu.pl>
2026-03-27 22:30:26 +0100

    dodanie manadzera urzadzenia ktory zarzadza peryferiami holtera

---
+++ Embedded Software/EROS- RTOS/lib/DeviceManager/DeviceManager.h
@@ -0,0 +1,60 @@
+#include <Arduino.h>
+#include "BluetoothSerial.h"
+#include "display.h"
+#include "HeartMonitor.h"
+#include "CsvWriter.h" 
+/*
+Okej no to ogolnie fa klasa dziala w ten sposob ze zbiera wszystkie urzadzenia peryferyjne i nimi zarzadza,
+mowi co sie dzieje i kiedy sie dzieje oraz na co maja reagowac przyciski. Jest po to zeby urzadzenie dzialalo 
+w miare sekwencyjnie czyli na tą chwile:
+- wlaczam urzadzenie i ma pokazac sie ekran ktory bedzie prostym menu wyboru czasu trwania badania
+- jesli wlacze urzadzenie i nie bedzie kart SD albo bedzie problem z systemem plikow, urzadzenie ma powiadamiac o tym fakcie na monitorze (trzeba uzupelnic o tą funkcjonalność funckje waitingForSDCard i dodac odpowiednie widoki ekranu w display.cpp zeby latwo tam je wywołać)
+- menu reaguje na 3 przyciski i mozna sie poruszac po nim: gora, dol oraz zatwierdz
+- po zatwierdzeniu czasu trwania badania urzadzenie przechodzi w tryb pracy na systemie czasu rzeczywistego
+- wybrany czas Badania jest zapisywany w zmiennej EKGegzamineTime po czym zostanie dodany do warunku stopu w funkcji EKGReadingAndSending();
+- funkcja EKGReadingAndSending() musi byc uzupelniona o obliczanie czasu (ile trwa badanie) przy uyciu millis() oraz na tej podstawie o warunek stopu
+- dzieki zmiennej wyrzucanej przez millis() zapamietanej w momencie startu badania mozna policzyc (na podstawie aktualnego millis()) ile czasu zostało do końca badania 
+- funkcja wyświetlająca głowny ekran urzadzenia podczas trwania badania zostanie uzupelniona o czas do końca badania na tej samej podstawie
+- powinien powstać ekran końcowy do ktorego przechodzi urzadzenie po zakonczeniu badania, informujacy o tym ze wynik mozna miec albo na aplikacji albo ze jest gotowy na karcie SD 
+- pelne funkcje wyswietlania ekranow znajduja sie w plikach display.cpp
+- bedzie trzeba uzupelnic funkcje checkBluetooth(), o mozliwosc masowego przesyłu danych gdy jest ona aktywna i ktos przylaczył sie do urzadznia (pacjent)
+- poki co mysle ze mozna zostawic konczenie badania przy nacisnieciu przycisku dla latwych testow urzadzenia.
+- trzeba jeszcze poprawic jakos funkcje odswierzania wyswietlacza zeby wykonywala sie szybciej i bez czarnego tla
+*/
+
+//definicje pinow do przyciskow
+#define BTN_BT 2
+#define BTN_LCD 21
+#define BTN_MIN 3
+
+class DeviceManager{
+    public:
+        volatile bool displayEnabled; //zmienna ustawiajaca sie na jeden jak wyswietlacz jest wzbudzony
+        volatile bool btEnabled; //zmienna ustawiajaca sie na jeden jak bluetooth przesyla
+
+        bool SDcardEnabled; //zmienna ktora ustawiona na 1 informauje ze karta jest wpięta
+        bool fileSystemEnabled;
+
+        //zmienne potrzebne do obliczania czasu trwania badania
+        uint8_t EKGegzamineTime;
+
+        unsigned long btPressStart = 0; //zmienna do zliczania czasu nacisku przycisku bluetooth
+        unsigned long lcdPressStart = 0; //zmienna do zliczania czasu nacisku przycisku wysweitlacza
+
+        BluetoothSerial SerialBT;
+        CsvWriter holter; //klasa zarzadzajaca holterem
+        //Epd epd; //klasa zarzadzajaca wyswietlaczem
+
+        DeviceManager();
+        void init();
+        void checkBluetooth();
+        void EKGReadingAndSending();
+        void updateDisplay(uint32_t timeInMs); //funkcja odświerzająca wyświetlacz tyle czasu ile trzeba (pozniej go wylacza) (czas podany w milisekundach)
+        void checkButtons(); //funkcja sprawdzajaca przyciski i ustawiajace displayEnabled oraz btEnabled
+        void waitingForSDcard(); //funkcja działająca w nieskończonej pętli zeby program nie szedl dalej puki nie bedzie karty SD
+
+    private:
+
+};
+
+
--- Embedded Software/EROS- RTOS/src/main.cpp
+++ Embedded Software/EROS- RTOS/src/main.cpp
@@ -1,232 +1,88 @@
 #include <Arduino.h>
-#include "BluetoothSerial.h"
-#include "display.h"
-#include <SD.h>
-#include "HeartMonitor.h"
-#include "CsvWriter.h" 
+#include "DeviceManager.h"
 
-BluetoothSerial SerialBT;
-CsvWriter holter; 
-Epd epd;
+/*
+BARDZIEJ SZCZEOLOWY OPIS DZIALANIA KODU I URZADZENIA JEST ZAMIESZCZEONY W PLIKU DEVICEMANAGER.H
+jest tam tez napisane co jeszcze na obecna chwile trzebaby dodac do tej klasy bo to jej metody zarzadzaja 
+urzadzeniem, tak aby tutaj w mainie obslugiwac samego RTOS i zeby kod tutaj byl ladny i czytelny.
+*/
 
-/* ===== PINY DO PRZYCISKOW ===== */
-
-#define BTN_BT 2
-#define BTN_LCD 21
-#define BTN_MIN 3
-
-/* ===== TASK HANDLE ===== */
-
+//uchwyty na taski
 TaskHandle_t measureTaskHandle;
 TaskHandle_t btTaskHandle;
 TaskHandle_t displayTaskHandle;
 
-/* ===== FLAGI ===== */
-
-bool btEnabled = false;
-bool displayEnabled = false;
-
-/* ===== TIMER BUTTON ===== */
-
-unsigned long btPressStart = 0;
-unsigned long lcdPressStart = 0;
+//uchwyty na mutexy
+SemaphoreHandle_t displayMutex;
+SemaphoreHandle_t btMutex;
+SemaphoreHandle_t sdMutex;
 
 
-/* ============================= */
-/* TASK 1 – POMIAR + SD */
-/* ============================= */
+//obiekt klasy DeviceManager zarzadzajacy wszystkim
+DeviceManager HolterDevice;
 
 void measureTask(void *parameter)
 {
     while (true)
     {
-        /* Kod do pomiarow na karte SD */
-        if (Serial.available()) {
-            char c = Serial.read();
-            if (c == 's' || c == 'S') {
-                if (holter.isRecording()) {
-                    holter.closeFile();
-                    Serial.println(">>> STOP: Plik zapisany i zamkniety! <<<");
-                }
-            }
+        if (xSemaphoreTake(sdMutex, portMAX_DELAY))
+        {
+            HolterDevice.EKGReadingAndSending();
+            xSemaphoreGive(sdMutex);
         }
-
-        processHeartRate();
-        if (holter.isRecording()) {
-            int16_t val = isLeadOff() ? 0 : (int16_t)getFilteredValue();
-            holter.writeSample(val, getAverageBPM(), isLeadOff());
-            
-            static unsigned long lastTick = 0;
-            if (millis() - lastTick > 1000) {
-                Serial.print(".");
-                lastTick = millis();
-            }
-        }
-        vTaskDelay(pdMS_TO_TICKS(4));
     }
 }
 
-
-/* ============================= */
-/* TASK 2 – BLUETOOTH */
-/* ============================= */
 
 void btTask(void *parameter)
 {
     while (true)
     {
-        if (btEnabled && SerialBT.hasClient())
+        if (xSemaphoreTake(btMutex, portMAX_DELAY))
         {
-            Serial.println("Wysylanie pliku przez BT");
-
-            /* TUTAJ KOD DO PRZESYLANIA PLIKU BLUETOOTH*/
-            // sendFileFromSD();
-
-            vTaskDelay(pdMS_TO_TICKS(5000));
+            HolterDevice.checkButtons();
+            HolterDevice.checkBluetooth();
+            vTaskDelay(pdMS_TO_TICKS(200));
+            xSemaphoreGive(btMutex);
         }
-
-        vTaskDelay(pdMS_TO_TICKS(200));
     }
+
 }
-
-
-/* ============================= */
-/* TASK 3 – DISPLAY */
-/* ============================= */
 
 void displayTask(void *parameter)
 {
-
     while (true)
     {
-        /* KOD DO WYSWIETLACZA*/
-        if (displayEnabled){
-            for(int i = 0; i<=5; i++){
-                display_battery(EMPTY);
-                display_bluetooth();
-                display_BPM(33+i);
-                displayTimeLeft(10,15);
-                //epd.DisplayFrame();
-                epd.DisplayFrame();
-
-                vTaskDelay(pdMS_TO_TICKS(400));
-            }
-            displayEnabled = false;
-            epd.Clear();
-
+        if (xSemaphoreTake(displayMutex, portMAX_DELAY))
+        {
+            HolterDevice.updateDisplay(3000);
+            xSemaphoreGive(displayMutex);
         }
-        vTaskDelay(pdMS_TO_TICKS(200));
     }
 }
-
-
-/* ============================= */
-/* OBSLUGA PRZYCISKOW */
-/* ============================= */
-
-void checkButtons()
-{
-    /* ===== BUTTON BLUETOOTH ===== */
-
-    static bool btTriggered = false;
-
-    if (digitalRead(BTN_BT) == LOW)
-    {
-        if (btPressStart == 0)
-            btPressStart = millis();
-
-        if (!btTriggered && millis() - btPressStart > 3000)
-        {
-            btEnabled = true;
-            btTriggered = true;
-            Serial.println("BT ENABLED");
-        }
-    }
-    else
-    {
-        btPressStart = 0;
-        btTriggered = false;
-    }
-
-
-    /* ===== BUTTON DISPLAY ===== */
-
-    static bool lcdTriggered = false;
-
-    if (digitalRead(BTN_LCD) == LOW)
-    {
-        if (lcdPressStart == 0)
-            lcdPressStart = millis();
-
-        if (!lcdTriggered && millis() - lcdPressStart > 2000)
-        {
-            displayEnabled = true;
-            lcdTriggered = true;
-            Serial.println("DISPLAY ENABLED");
-        }
-    }
-    else
-    {
-        lcdPressStart = 0;
-        lcdTriggered = false;
-    }
-}
-
-
-/* ============================= */
-/* SETUP */
-/* ============================= */
 
 void setup()
 {
-    Serial.begin(115200);
+    //Inicjalizacja wszystkich peryferiow znajduje sie w metodzie init dla klasy device manager
+    HolterDevice.init();
 
-    //PINOUT
-    pinMode(BTN_BT, INPUT_PULLUP);
-    pinMode(BTN_LCD, INPUT_PULLUP);
+    //inicjalizacja mutexow
+    displayMutex = xSemaphoreCreateMutex();
+    btMutex = xSemaphoreCreateMutex();
+    sdMutex = xSemaphoreCreateMutex();
 
-    //===== INICJALIZACJE PERYFERIOW =========
+    //czekanie na karte SD
+    HolterDevice.waitingForSDcard();
 
-    //bluetooth
-    SerialBT.begin("ESP32_logger");
+    // tutaj fajnie by bylo stworzyc funkcje ktora ustawia czas trwania badania (to jest pole EKGEgzamineTime)
+    // za pomocą wyświetlania prostego menu wyświetlacza i poruszania sie po nim za pomoca przyciskow.
 
-    //wyswietlacz
-    epd.LDirInit();
-    epd.Clear();
-    delay(100);
-
-    //EKG
-    initHeartMonitor();
-    
-    //karta SD
-    Serial.println("Initializing SD card.");
-    if (!SD.begin()) {
-        Serial.println("ERROR: SD card does not work properly.");
-    }
-
-    if (holter.begin("/test_ekg.csv")) {
-        Serial.println("File opened, press 's' to stop. Writing data...");
-    } else {
-        Serial.println("ERROR: File system dumped writing to the file.");
-    }
-
-    /* ===== TWORZENIE TASKOW ===== */
-
+    //tworzenie taskow
     xTaskCreatePinnedToCore(measureTask,"Measure Task",4096,NULL,2,&measureTaskHandle,1);
-
     xTaskCreatePinnedToCore(btTask,"BT Task",4096,NULL,1,&btTaskHandle,0);
-
     xTaskCreatePinnedToCore(displayTask,"Display Task",8192,NULL,1,&displayTaskHandle,0);
-
 }
-
-/* ============================= */
-/* LOOP */
-/* ============================= */
-
-void loop()
-{
-    checkButtons();
-
-    vTaskDelay(pdMS_TO_TICKS(100));
+void loop(){
+    vTaskDelay(pdMS_TO_TICKS(200));
+    //pusto bo tutaj nie ma co wstawiac
 }
