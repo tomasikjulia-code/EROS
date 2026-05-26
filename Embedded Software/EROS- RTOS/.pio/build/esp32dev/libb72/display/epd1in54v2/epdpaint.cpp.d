6e9154e68f2c846fd297bee1e831c0db5c91a130
Paweł Czarzasty <278356@student.pwr.edu.pl>
2026-05-26 23:38:16 +0200

    dodano szybki skok w celu przemieszczenia kursora w wysylaniu pliku. dodano rowniez mozliwosc usuwania badania z karty SD z aplikacji oraz usuwanie przy kazdym wlaczeniu urządzenia.

---
+++ Embedded Software/EROS- RTOS/.pio/build/esp32dev/libb72/display/epd1in54v2/epdpaint.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/libb72/display/epd1in54v2/epdpaint.cpp.o: \
+ lib/display/epd1in54v2/epdpaint.cpp \
+ /Users/pawel/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h \
+ lib/display/epd1in54v2/epdpaint.h lib/display/epd1in54v2/fonts.h
