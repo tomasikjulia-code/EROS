b2ee1724210aa0ecd3b78d6b3c768e3aad09797d
Maciej Dzimira <maciej.dzimira@comarch.com>
2026-06-15 10:55:30 +0200

    APP aplikacja do stawienia lokalnie

--- MobileApp/Frontend/metro.config.js
+++ MobileApp/Frontend/metro.config.js
@@ -1,7 +1,23 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
 config.resolver.assetExts.push('txt', 'csv');
+
+// Redirect native-only modules to web stubs when bundling for web
+const originalResolveRequest = config.resolver.resolveRequest;
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (platform === 'web' && moduleName === 'react-native-fs') {
+    return {
+      filePath: path.resolve(__dirname, 'src/mocks/react-native-fs.web.js'),
+      type: 'sourceFile',
+    };
+  }
+  if (originalResolveRequest) {
+    return originalResolveRequest(context, moduleName, platform);
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
 
 module.exports = config;
