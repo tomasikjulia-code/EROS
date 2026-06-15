88670b804f978ea632a4f8fe46a26c7461088cfe
Maciej Dzimira <maciej.dzimira@comarch.com>
2026-06-15 21:22:53 +0200

    WEB2 poprawki builda

--- Website2/vite.config.js
+++ Website2/vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: { historyApiFallback: true },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules/three/')) return 'three';
+          if (id.includes('@react-three/')) return 'react-three';
+          if (id.includes('node_modules/react') || id.includes('node_modules/react-dom')) return 'react';
+        },
+      },
+    },
+  },
 })
